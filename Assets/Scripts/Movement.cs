017be59f7d7aa65fcd0ff763c0980522be92f9df
Magicman <clement.ciranna@viacesi.fr>
2021-01-30 01:32:51 +0100

    test mlti

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Mirror;
+
+public class Movement : NetworkBehaviour
+{
+
+    [SerializeField] private Vector3 movement = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    [Client]
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasAuthority)
+        {
+            return;
+        }
+        
+        if(!Input.GetKeyDown(KeyCode.UpArrow)) { return; }
+
+        transform.Translate(movement);
+        
+    }
+}
