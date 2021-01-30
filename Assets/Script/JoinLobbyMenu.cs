16e1f51df26b57d0a899f5e41af7892770bcea45
Ma6icm4n <72262365+Ma6icm4n@users.noreply.github.com>
2021-01-30 15:05:00 +0100

    Add Project Main Menu and Multiplayer

---
+++ Assets/Script/JoinLobbyMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class JoinLobbyMenu : MonoBehaviour
+{
+    [SerializeField] private NetworkManagerLobby networkManager = null;
+
+    [Header("UI")]
+    [SerializeField] private GameObject LandingPagePanel = null;
+    [SerializeField] private TMP_InputField ipAdressInputField = null;
+    [SerializeField] private Button joinButton = null;
+
+    private void OnEnable()
+    {
+        NetworkManagerLobby.OnClientConnected += HandleClientConnected;
+        NetworkManagerLobby.OnClientDisconnected += HandleClientDisconnected;
+    }
+
+
+    private void OnDisable()
+    {
+        NetworkManagerLobby.OnClientConnected -= HandleClientConnected;
+        NetworkManagerLobby.OnClientDisconnected -= HandleClientDisconnected;
+    }
+
+    public void JoinLobby()
+    {
+        string ipAddress = ipAdressInputField.text;
+
+        networkManager.networkAddress = ipAddress;
+        networkManager.StartClient();
+
+        joinButton.interactable = false;
+    }
+
+    private void HandleClientConnected()
+    {
+        joinButton.interactable = true;
+
+        gameObject.SetActive(false);
+        LandingPagePanel.SetActive(false);
+    }
+
+    private void HandleClientDisconnected()
+    {
+        joinButton.interactable = true;
+    }
+}
